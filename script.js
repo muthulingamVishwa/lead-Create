3f5cb9d02f86b95318db0b443e0ae6c53525ec62
muthulingamVishwa <muthulingam.vishwa@gmail.com>
2024-03-24 04:05:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function ch(event){
+    let words=event.target.value;
+      console.log(words);
+    let outputElement=document.getElementById('out');
+    outputElement.innerHTML=words;
+}
+function timestamp() { 
+    var response = document.getElementById("g-recaptcha-response"); 
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
+     } } 
+     setInterval(timestamp, 500); 
+
+function capthcs(){
+
+
+}
